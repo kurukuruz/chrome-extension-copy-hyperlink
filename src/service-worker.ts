3606f4eb73908be8dd17f0790b933fd4aeb9471a
kurukuruz <kurukuruz@local>
2025-07-02 21:13:41 +0900

    modify: undefinedチェックを追加

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,6 +1,9 @@
 chrome.action.onClicked.addListener((tab) => {
+  if (!tab.id) {
+    throw new Error("Tab ID is not available");
+  }
   chrome.scripting.executeScript({
-    target: { tabId: tab.id! },
+    target: { tabId: tab.id },
     files: ["content.js"],
   });
 });
